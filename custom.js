b595f9f74a8821fd7ef4ad3a9e7790054c96b603
deniskondrachuk <deniskondrachuk@gmail.com>
2017-01-11 11:43:40 +0200

    last fix

---
+++ custom.js
@@ -0,0 +1,10 @@
+/**
+ * Created by deniskondrachuk on 04.01.17.
+ */
+$(document).ready(function(){
+    $('.parallax').parallax();
+});
+
+function deletePoints () {
+    $('#frame').removeClass('frame')
+}
